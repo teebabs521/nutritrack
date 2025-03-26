326836975c6f891e7cebaa08c421566e4feabf07
Ibrahim Tijani <144402297+teebabs521@users.noreply.github.com>
2025-03-26 16:46:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/nutritrack",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
 });
